eb412103185bf4afb94db3209c2bfa94e8a76ef9
Yuri Aleksandrov <george2code@gmail.com>
2016-10-24 09:27:50 +0300

    ывыф

---
+++ MusicVideo/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  MusicVideo
+//
+//  Created by Yuri on 10/19/16.
+//  Copyright © 2016 fusta. All rights reserved.
+//
+
+import Foundation
+
+typealias JSONDictionary = [String: AnyObject]
+typealias JSONArray = Array<AnyObject>.Element
